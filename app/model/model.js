f6745f718ac00aeb71e6dfd4f87833f1cbe6ce5e
Marcos Schneider <marcos.schneider13@hotmail.com>
2019-11-14 14:47:54 -0300

    alteracoes model

--- app/model/model.js
+++ app/model/model.js
@@ -2,18 +2,18 @@
 var crypto = require('crypto');
 
 // FAZER LEITURA DAS CONFIGURAÇÕES
+// var config = {
+// 						    "host"     : "localhost",
+// 						    "user"     : "root",
+// 						    "password" : "root",
+// 						    "database" : "hadassah"
+// 					  	};
 var config = {
-						    "host"     : "localhost",
-						    "user"     : "root",
-						    "password" : "root",
+						    "host"     : "mysql669.umbler.com",
+						    "user"     : "admin-hadassah",
+						    "password" : "GSqb}6(2dB)4",
 						    "database" : "hadassah"
 					  	};
-// var config = {
-// 						    "host"     : "us-cdbr-iron-east-05.cleardb.net",
-// 						    "user"     : "bc9f7435f43c91",
-// 						    "password" : "d6c51f9d",
-// 						    "database" : "heroku_9d1276cf5a6af09"
-// 					  	};
 
 // // CONEXÃO MYSQL
 var mysql      = require('mysql');
